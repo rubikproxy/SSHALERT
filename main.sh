724efa2c08a45666e70d19417f10ba7202d693d1
SANJAY KUMAR <84948167+rubikproxy@users.noreply.github.com>
2024-11-05 19:24:44 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,6 @@
 sudo apt install rsyslog python3 python3-pip
+sudo mkdir var/log/SSH-Alert
+sudo touch /var/log/SSH-Alert/sshalert.log
 sudo ln -s /ssh_login_alert/sshalert.service /etc/systemd/system/sshalert.service
 sudo ln -s /ssh_login_alert/sshalert /etc/init.d/sshalert
 sudo chmod +x /etc/init.d/sshalert
