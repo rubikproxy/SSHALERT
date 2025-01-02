afaf5a41604ccb0b15fd94048f501e5d83b81a85
SANJAY KUMAR <84948167+rubikproxy@users.noreply.github.com>
2025-01-02 08:12:11 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -66,11 +66,3 @@ install_dependencies
 setup_service
 
 echo "Setup completed successfully."
-
-
-# sudo apt install rsyslog python3 python3-pip
-# sudo mkdir var/log/SSH-Alert
-# sudo touch /var/log/SSH-Alert/sshalert.log
-# sudo ln -s /ssh_login_alert/sshalert.service /etc/systemd/system/sshalert.service
-# sudo ln -s /ssh_login_alert/sshalert /etc/init.d/sshalert
-# sudo chmod +x /etc/init.d/sshalert
